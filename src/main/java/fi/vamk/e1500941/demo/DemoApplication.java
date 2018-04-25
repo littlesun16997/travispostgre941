dfbaae1356fefc2b36d5aac6c61b6eb755cf9e3c
e1500941 <e1500941@edu.vamk.fi>
2018-04-25 16:40:52 +0300

    Update demo app

--- src/main/java/fi/vamk/e1500941/demo/DemoApplication.java
+++ src/main/java/fi/vamk/e1500941/demo/DemoApplication.java
@@ -2,10 +2,18 @@ package fi.vamk.e1500941.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	@RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Hello World!";
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
